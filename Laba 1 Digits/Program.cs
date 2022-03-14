388d8155e02deee2d99fda6aaacb2f666f6fbe29
GreatSea1 <user.name>
2022-03-14 11:01:02 +0700

    Bebrium

---
+++ Laba 1 Digits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Number, RightPart, Digit, NewNumber;
+
+            Console.Write("Type a Number ");
+            Number = Console.ReadLine();
+
+            RightPart = Number.Substring(2, Number.Length - 2);
+            Digit = Number.Substring(1, (Number.Length - (Number.Length - 1)));
+            NewNumber = RightPart + Digit;
+
+            Console.WriteLine("Right Part of the Number is '{0}' ", RightPart);
+            Console.WriteLine("Second Digit of the Number is '{0}' ", Digit);
+            Console.WriteLine("New Number is '{0}' ", NewNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba 1 Powers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1_Powers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number; int Power;
+
+            Console.WriteLine("Type a Number ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            int Result = Number;
+
+            Console.WriteLine("Type a Power ");
+            Power = Convert.ToInt32(Console.ReadLine());
+
+            for (int CountPower = 1; CountPower < Power; ++CountPower)
+            {
+                Result = Result * Number;
+            }
+            Console.WriteLine("Result is " + Result);
+            Console.ReadKey();
+        }
+    }
+}
